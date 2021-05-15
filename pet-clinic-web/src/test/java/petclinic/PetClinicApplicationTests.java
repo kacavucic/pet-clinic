99152fa3de1c14a3971fabc995a38d749dafe8f2
kacavucic <vucic.kat@gmail.com>
2021-05-15 05:14:10 +0200

    Refactored to multi-module maven project. Closes #1.

--- pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/petclinic/PetClinicApplicationTests.java
@@ -1,16 +1,17 @@
-package com.springframework.petclinic;
+package petclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class PetClinicApplicationTests {
+public class PetClinicApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
