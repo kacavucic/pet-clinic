91c08e9920b394328a80002f6d9a38cc15923c66
kacavucic <vucic.kat@gmail.com>
2021-05-17 03:30:12 +0200

    Added spring config. Closes #22.

--- pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/services/map/OwnerServiceMap.java
@@ -2,11 +2,12 @@ package com.springframework.petclinic.services.map;
 
 import com.springframework.petclinic.model.Owner;
 import com.springframework.petclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
-
 
     @Override
     public Set<Owner> findAll() {
